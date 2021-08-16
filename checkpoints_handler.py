6c0ae30212daec39cc0edfb083e1474cf01d2f79
n1k0din <nik726@gmail.com>
2021-08-16 13:51:34 +0700

    Add parser, validator, logging and tcp handler

---
+++ checkpoints_handler.py
@@ -0,0 +1,127 @@
+"""
+Необходимо сделать tcp сервер, который распознаёт заданный формат данных и отображает его в
+требуемом формате. Обязательна запись данных во внешний файл. Интерфейс и способ отображения на
+выбор разработчика.
+
+Формат данных BBBBxNNxHH:MM:SS.zhqxGGCR Где
+BBBB - номер участника
+x - пробельный символ
+NN - id канала
+HH - Часы
+MM - минуты
+SS - секунды
+zhq - десятые сотые тысячные
+GG - номер группы
+CR - «возврат каретки» (закрывающий символ)
+
+Пример данных: 0002 C1 01:13:02.877 00[CR] Выводим «спортсмен, нагрудный номер BBBB прошёл отсечку
+NN в «время»" до десятых, сотые и тысячные отсекаются. Только для группы 00.
+Для остальных групп данные не отображаются, но пишутся в лог полностью.
+
+Язык Python, версия не ниже 3.2, не выше 3.8.
+Передача данных должна поддерживаться с помощью telnet клиента.
+"""
+import json
+import logging
+import os
+import re
+import socketserver
+from collections import namedtuple
+
+Datagram = namedtuple(
+    'Datagram',
+    [
+        'competitor_number',
+        'channel_id',
+        'hours',
+        'minutes',
+        'seconds',
+        'tenths',
+        'hundredths',
+        'thousandths',
+        'group_number',
+    ]
+)
+
+
+class CheckpointsHandler(socketserver.StreamRequestHandler):
+
+    def handle(self):
+        while True:
+            input = self.rfile.readline().decode()
+
+            if is_datagram_valid(input):
+                datagram = datagram_parse(input)
+                dump_datagram(datagram)
+
+                if filter_by_group(datagram, '00'):
+                    print_datagram(datagram)
+            else:
+                dump_invalid_data(input)
+
+
+def is_datagram_valid(datagram_string):
+    pattern = re.compile(r'\d{4}\s.{2}\s\d{2}:\d{2}:\d{2}\.\d{3}\s\d{2}\r$')
+    return bool(pattern.match(datagram_string))
+
+
+def datagram_parse(datagram_string):
+    competitor_number, channel_id, time, group_number = datagram_string.rstrip().split()
+    hhmmss, zhq = time.split('.')
+    hours, minutes, seconds = hhmmss.split(':')
+    tenths, hundredths, thousandths = zhq
+
+    return Datagram(
+        competitor_number,
+        channel_id,
+        hours,
+        minutes,
+        seconds,
+        tenths,
+        hundredths,
+        thousandths,
+        group_number,
+    )
+
+
+def print_datagram(datagram):
+    template = 'спортсмен, нагрудный номер {} прошёл отсечку {} в {}:{}:{}.{}'
+    print(template.format(
+        datagram.competitor_number,
+        datagram.channel_id,
+        datagram.hours,
+        datagram.minutes,
+        datagram.seconds,
+        datagram.tenths,
+    ))
+
+
+def dump_datagram(datagram):
+    logging.info(json.dumps(datagram._asdict()))
+
+
+def dump_invalid_data(invalid_string):
+    logging.warning('INVALID %s', invalid_string.strip())
+
+
+def filter_by_group(datagram, group_number):
+    return datagram.group_number == group_number
+
+
+def main():
+    logfilename = 'competitors.log'
+    logging.basicConfig(
+        filename=logfilename,
+        format='%(asctime)s %(levelname)s:%(message)s',
+        level=logging.INFO,
+    )
+
+    port = int(os.environ.get('PORT', 8000))
+    host = os.environ.get('HOST', '')
+
+    with socketserver.TCPServer((host, port), CheckpointsHandler) as server:
+        server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
